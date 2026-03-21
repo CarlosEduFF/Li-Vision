983fbdf9d5322fd441c934ff19dcb5446631fbff
CarlosEduFF <carlos.farias11@etec.sp.gov.br>
2026-03-21 18:08:19 -0300

    Fix Cam format e uri

--- app/screens/cam.tsx
+++ app/screens/cam.tsx
@@ -8,35 +8,62 @@ export default function CameraScreen() {
   const cameraRef = useRef<CameraView | null>(null);
   const [gesture, setGesture] = useState<string | null>(null);
 
-  const handleDetection = async () => {
-    const result = await detectGesture();
-    setGesture(result);
+  const startDetection = async () => {
+    if (!cameraRef.current) return;
+
+
+
+    const interval = setInterval(async () => {
+      const photo = await cameraRef.current?.takePictureAsync({
+        base64: true,
+        quality: 0.5,
+        skipProcessing: true
+      });
+      if (!photo) return;
+      const result = await detectGesture(photo.uri);
+      setGesture(result);
+
+    }, 500); // 2 FPS
+
+    return () => clearInterval(interval);
   };
 
   return (
     <View style={styles.container}>
-      <CameraView
-        ref={cameraRef}
-        style={styles.camera}
-      />
+      <View style={styles.cameraContainer}>
+        <CameraView
+          ref={cameraRef}
+          style={styles.camera}
+          facing="front"
+        />
 
-      {gesture && (
-        <Text style={styles.gestureText}>
-          Gesto: {gesture}
-        </Text>
-      )}
-    </View>
+        {gesture && (
+          <Text style={styles.gestureText}>
+            Gesto: {gesture}
+          </Text>
+        )}
+      </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
-  container:{ flex:1 },
-  camera:{ flex:1 },
-  gestureText:{
-    position:"absolute",
-    bottom:50,
-    alignSelf:"center",
-    fontSize:24,
-    color:"white"
+  container: { flex: 1 },
+  cameraContainer: {
+    height: 300,
+    margin: 20,
+    borderRadius: 20,
+    overflow: "hidden"
+  },
+
+  camera: {
+    flex: 1
+  },
+  gestureText: {
+    position: "absolute",
+    bottom: 50,
+    alignSelf: "center",
+    fontSize: 24,
+    color: "white"
   }
 });
--- services/gestureService.ts
+++ services/gestureService.ts
@@ -1,10 +1,21 @@
-export async function detectGesture(): Promise<string> {
+export async function detectGesture(uri: string): Promise<string> {
 
-  const response = await fetch("http://localhost:8000/detect", {
-    method: "POST"
+  const form = new FormData();
+
+  form.append("file", {
+    uri,
+    name: "frame.jpg",
+    type: "image/jpeg"
+  } as any);
+
+  const response = await fetch("https://li-vision.onrender.com/detect", {
+    method: "POST",
+    body: form,
+    headers: {
+      "Content-Type": "multipart/form-data"
+    }
   });
 
   const data = await response.json();
-
   return data.gesture;
 }
