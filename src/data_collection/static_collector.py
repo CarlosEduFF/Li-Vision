99cec65f6bfa0ff416a15e4973b646c16dc77ae6
CarlosEduFF <carlos.farias11@etec.sp.gov.br>
2026-04-18 09:31:53 -0300

    fix: recording recogniza and savement

--- src/data_collection/static_collector.py
+++ src/data_collection/static_collector.py
@@ -12,9 +12,11 @@ class StaticCollector:
         (21 points x 2 relative coordinates).
         The coordinates are relative to the bounding box of the hand.
         """
+        landmarks = hand.landmark if hasattr(hand, "landmark") else hand
+        
         x_calc = []
         y_calc = []
-        for landmark in hand.landmark:
+        for landmark in landmarks:
             x_calc.append(landmark.x)
             y_calc.append(landmark.y)
 
@@ -23,7 +25,7 @@ class StaticCollector:
         width, height = x_max - x_min, y_max - y_min
 
         features = []
-        for landmark in hand.landmark:
+        for landmark in landmarks:
             # relative x and y
             rel_x = (landmark.x - x_min) / width if width > 0 else 0
             rel_y = (landmark.y - y_min) / height if height > 0 else 0
